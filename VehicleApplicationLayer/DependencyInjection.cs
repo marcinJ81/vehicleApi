3413442bbbb814a9ff89f2d6a489540e46bc20b8
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2024-12-02 22:42:54 +0100

    add migration

--- VehicleApplicationLayer/DependencyInjection.cs
+++ VehicleApplicationLayer/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using FluentValidation.AspNetCore;
+using Infrastructure.Entity.Context;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -23,7 +25,9 @@ namespace VehicleApplicationLayer
                  v.ImplicitlyValidateRootCollectionElements = true;
                  v.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
              });
-
+            services.AddDbContext<VehicleDbContext>(
+               options => options.UseSqlServer(configuration.GetConnectionString("MsSqlDB"))
+           );
             //register db contesxt
             //Register other services and interfaces
             return services;
