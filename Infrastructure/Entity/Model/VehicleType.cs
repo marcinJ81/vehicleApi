4192c464381b51c0d03e7febecfd0712be39aa96
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2024-12-02 21:51:45 +0100

    add models

---
+++ Infrastructure/Entity/Model/VehicleType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entity.Model
+{
+    public class VehicleType
+    {
+        [Key]
+        public int VehicleType_Id { get; set; }
+        public string VehicleType_Name { get; set; }
+        public ICollection<Vehicle> Vehicles { get; set; }
+    }
+}
