39f2d6e259095dca7a41affc2f0a5ab9de233455
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2024-12-13 18:57:45 +0100

    Revert "change class and prop"
    This reverts commit dd12bc7e990b3e55cb5d85fc14c073b9e8016232.

--- Infrastructure/test-class.cs
+++ Infrastructure/test-class.cs
@@ -9,7 +9,6 @@ namespace Infrastructure
     internal class test_class
     {
         public int MyProperty { get; set; }
-        public int test { get; set;}
-        public string testString { get; set; }
+        public int MyProperty2 { get; set;}
     }
 }
