0c0817612ab7fc5ed0992a6b1120ff3b0badaf14
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2024-12-29 11:28:55 +0100

    Revert "Merge branch 'fix/test'"
    This reverts commit 8fbd3587d77167ad6e8a5d0689d5b12737624836, reversing
    changes made to 7e6e4a3b2ebf8f897f1d833748ad77900e2b5273.

--- Infrastructure/test-class.cs
+++ Infrastructure/test-class.cs
@@ -18,11 +18,4 @@ namespace Infrastructure
         public int MyProperty { get; set; } 
         public int MyProperty2 { get; set;}
     }
-
-    public class Test_class3
-    {
-        public int MyProperty { get; set; }
-        public int MyProperty1 { get; set; }
-        public int MyProperty2 { get; set; }
-    }
 }
