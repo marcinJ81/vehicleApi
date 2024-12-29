40ca1d91ad0f384a691cc7c104df0132ba156abb
Marcin Juranek <34885638+marcinJ81@users.noreply.github.com>
2024-12-29 17:56:05 +0100

    add class

---
+++ Infrastructure/TestClasses.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    internal class TestClasses
+    {
+        public int MyProperty { get; set; }
+    }
+    class MyClass
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+    }
+
+    class MyClass1
+    {
+        public int MyProperty { get; set; }
+        public int MyProperty1 { get; set; }
+        public int MyProperty2 { get; set; }
+    }
+}
